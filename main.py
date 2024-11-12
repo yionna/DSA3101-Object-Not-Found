8b30806fb157b65c0e88c61b3901948232a4f0f0
Mingyuan Zhang <mingyuanzhang@MYs-Macbook.local>
2024-11-12 16:58:15 +0800

    Updated main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+import pandas as pd
+from models.customerSegmentation.static import *
+from models.customerSegmentation.CustomerDataGenerator import *
 
+# Importing data
+base_df = pd.read_csv("data/processed/BankChurners_more.csv")
+banking_df = pd.read_csv("data/processed/banking_behaviour_preference.csv")
+
+# Customer segmentation
+segmentation_test = CustomerSegmentation(banking_df)
+segmentation_result = segmentation_test.perform_segmentation()
