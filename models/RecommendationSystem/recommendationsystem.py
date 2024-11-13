d625c5537c47c376ecaf315bede2ff9bb7a08fc6
annccyn <annaccyn@gmail.com>
2024-11-13 21:09:38 +0800

    updates to main

--- models/RecommendationSystem/recommendationsystem.py
+++ models/RecommendationSystem/recommendationsystem.py
@@ -1,4 +1,7 @@
+import numpy as np
 import pandas as pd
+
+from sklearn.feature_selection import SelectKBest, f_regression
 from sklearn.model_selection import train_test_split
 from xgboost import XGBClassifier
 
